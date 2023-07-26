7b2dad56729dd099c39986ce549f58cb035ebfb1
Richard Hung <richardhunghhw@gmail.com>
2023-07-26 21:28:03 +0000

    Temporary bugfix for server side sentry bug

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -8,16 +8,16 @@ import type { EntryContext } from '@remix-run/cloudflare';
 import { RemixServer } from '@remix-run/react';
 import isbot from 'isbot';
 import { renderToReadableStream } from 'react-dom/server';
-import * as Sentry from '@sentry/remix';
+// import * as Sentry from '@sentry/remix';
 
-Sentry.init({
-  debug: __sentryDebug__,
-  dsn: '__sentryDsn__',
-  environment: '__sentryEnv__',
-  integrations: [],
-  tracePropagationTargets: ['/^.*.palmier.workers.dev/', 'localhost', /^\//],
-  tracesSampleRate: __sentryTracesSampleRate__,
-});
+// Sentry.init({
+//   debug: __sentryDebug__,
+//   dsn: '__sentryDsn__',
+//   environment: '__sentryEnv__',
+//   integrations: [],
+//   tracePropagationTargets: ['/^.*.palmier.workers.dev/', 'localhost', /^\//],
+//   tracesSampleRate: __sentryTracesSampleRate__,
+// });
 
 export default async function handleRequest(
   request: Request,
