5eb962be49fc6342f5c764dbdd1db9d254a478c8
Richard Hung <richardhunghhw@gmail.com>
2023-08-22 16:12:09 +0000

    Site manifest re-org

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export const links: LinksFunction = () => {
     { rel: 'apple-touch-icon', href: '/favicon/apple-touch-icon.png', sizes: '180x180' },
     { rel: 'icon', href: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     { rel: 'icon', href: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-    { rel: 'manifest', href: '/favicon/site.webmanifest' },
+    { rel: 'manifest', href: '/site.webmanifest' },
     { rel: 'stylesheet', href: stylesheet },
     {
       rel: 'preload',
